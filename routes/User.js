bf6e8024e190347e95560d8c23d6a3b1bf2a767e
Aishendo <89102440+Aishendo@users.noreply.github.com>
2022-05-28 12:00:33 +0600

    Update and rename User.js to GamesRoute.js

--- routes/User.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const UserController = require('../controllers/UserController')
-const router = express.Router();
-router.post('/user/register', UserController.register);
-router.post('/user/login', UserController.login);
-module.exports = router
